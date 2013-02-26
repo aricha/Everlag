cf53b1a5466fc914b0db07240a793f41d6de1b0a
Andrew Richardson <andrewr114@gmail.com>
2013-02-25 22:32:34 -0800

    Working fix for extreme lag: -Wrote a technique to replace all instances of [UIColor colorWithPatternImage:] with tiled UIImageViews, but it didn't help as much as I'd thought it would -A simple use of CALayer's shadowPath property on the note container view was enough to improve performance by 100000000000x

--- Everlag/Everlag.mm
+++ Everlag/Everlag.mm
@@ -3,88 +3,93 @@
 //  Everlag
 //
 //  Created by Andrew Richardson on 2013-02-25.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2013. All rights reserved.
 //
 
-// CaptainHook by Ryan Petrich
-// see https://github.com/rpetrich/CaptainHook/
-
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "CaptainHook/CaptainHook.h"
-#include <notify.h> // not required; for examples only
+#import <QuartzCore/QuartzCore.h>
 
-// Objective-C runtime hooking using CaptainHook:
-//   1. declare class using CHDeclareClass()
-//   2. load class using CHLoadClass() or CHLoadLateClass() in CHConstructor
-//   3. hook method using CHOptimizedMethod()
-//   4. register hook using CHHook() in CHConstructor
-//   5. (optionally) call old method using CHSuper()
+CHDeclareClass(UIColor)
+CHDeclareClass(UIView)
 
-
-@interface Everlag : NSObject
-
+@interface UIColor ()
+@property (nonatomic, retain, getter = _patternImage, setter = _setPatternImage:) UIImage *patternImage;
 @end
 
-@implementation Everlag
+@interface UIView ()
+@property (nonatomic, retain) UIImageView *backgroundImageView;
+@end
 
--(id)init
+CHPropertyRetainNonatomic(UIColor, UIImage *, _patternImage, _setPatternImage);
+
+CHOptimizedMethod1(self, id, UIColor, initWithPatternImage, UIImage *, pattern)
 {
-	if ((self = [super init]))
-	{
+	if ((self = CHSuper1(UIColor, initWithPatternImage, pattern))) {
+		self.patternImage = [pattern resizableImageWithCapInsets:UIEdgeInsetsZero];
 	}
-
-    return self;
+	return self;
 }
 
-@end
+CHPropertyRetainNonatomic(UIView, UIImageView *, backgroundImageView, setBackgroundImageView);
 
-
-@class ClassToHook;
-
-CHDeclareClass(ClassToHook); // declare class
-
-CHOptimizedMethod(0, self, void, ClassToHook, messageName) // hook method (with no arguments and no return value)
+CHOptimizedMethod1(self, void, UIView, setBackgroundColor, UIColor *, color)
 {
-	// write code here ...
-	
-	CHSuper(0, ClassToHook, messageName); // call old (original) method
+	UIColor *colorToUse = color;
+	UIImage *patternImage = color.patternImage;
+	if (color && patternImage) {
+		colorToUse = nil;
+		UIImageView *patternImageView = [[[UIImageView alloc] initWithImage:patternImage] autorelease];
+		patternImageView.frame = self.bounds;
+		patternImageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+		self.backgroundImageView = patternImageView;
+		[self addSubview:patternImageView];
+		[self sendSubviewToBack:patternImageView];
+	}
+	CHSuper1(UIView, setBackgroundColor, colorToUse);
 }
 
-CHOptimizedMethod(2, self, BOOL, ClassToHook, arg1, NSString*, value1, arg2, BOOL, value2) // hook method (with 2 arguments and a return value)
+CHOptimizedMethod1(self, void, UIView, sendSubviewToBack, UIView *, view)
 {
-	// write code here ...
-
-	return CHSuper(2, ClassToHook, arg1, value1, arg2, value2); // call old (original) method and return its return value
+	CHSuper1(UIView, sendSubviewToBack, view);
+	
+	UIImageView *bgView = self.backgroundImageView;
+	if (bgView && view && bgView != view) {
+		[self sendSubviewToBack:bgView];
+	}
 }
 
-static void WillEnterForeground(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+#pragma mark - Evernote
+
+CHDeclareClass(ENScalingFlipViewController)
+
+CHOptimizedMethod0(self, void, ENScalingFlipViewController, _postAnimationCleanup) // currently where shadow is set
 {
-	// not required; for example only
+	CHSuper0(ENScalingFlipViewController, _postAnimationCleanup);
+	
+	UIView **shadowView = CHIvarRef(self, _shadowView, UIView *);
+	if (shadowView) {
+		[*shadowView layer].shadowPath = [UIBezierPath bezierPathWithRect:[*shadowView bounds]].CGPath;
+	}
 }
 
-static void ExternallyPostedNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+CHConstructor
 {
-	// not required; for example only
-}
-
-CHConstructor // code block that runs immediately upon load
-{
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
-	// listen for local notification (not required; for example only)
-	CFNotificationCenterRef center = CFNotificationCenterGetLocalCenter();
-	CFNotificationCenterAddObserver(center, NULL, WillEnterForeground, CFSTR("UIApplicationWillEnterForegroundNotification"), NULL, CFNotificationSuspensionBehaviorCoalesce);
-	
-	// listen for system-side notification (not required; for example only)
-	// this would be posted using: notify_post("com.arichardson.Everlag.eventname");
-	CFNotificationCenterRef darwin = CFNotificationCenterGetDarwinNotifyCenter();
-	CFNotificationCenterAddObserver(darwin, NULL, ExternallyPostedNotification, CFSTR("com.arichardson.Everlag.eventname"), NULL, CFNotificationSuspensionBehaviorCoalesce);
-	
-	// CHLoadClass(ClassToHook); // load class (that is "available now")
-	// CHLoadLateClass(ClassToHook);  // load class (that will be "available later")
-	
-	CHHook(0, ClassToHook, messageName); // register hook
-	CHHook(2, ClassToHook, arg1, arg2); // register hook
-	
-	[pool drain];
+	@autoreleasepool {
+		// note: this stuff improves performance only marginally, so it's not needed right now
+//		CHLoadClass(UIColor);
+//		
+//		CHHookProperty(UIColor, _patternImage, _setPatternImage);
+//		CHHook1(UIColor, initWithPatternImage);
+//		
+//		CHLoadClass(UIView);
+//		
+//		CHHookProperty(UIView, backgroundImageView, setBackgroundImageView);
+//		CHHook1(UIView, setBackgroundColor);
+//		CHHook1(UIView, sendSubviewToBack);
+		
+		CHLoadLateClass(ENScalingFlipViewController);
+		CHHook0(ENScalingFlipViewController, _postAnimationCleanup);
+	}
 }
